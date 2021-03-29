6863bee9f2a658f43a6631b870cbac6e35882a6c
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2021-03-29 17:32:30 +0100

    Set priority of last patch

--- scratch/simple_req.el
+++ scratch/simple_req.el
@@ -1,16 +1,33 @@
 (require 'request)
 
 (setq api_key (getenv "EVG_API_KEY"))
-(if (= (length api_key) 0)
-      (message "Please make sure to set the env variable $EVG_API_KEY")
-(request
- "https://evergreen.mongodb.com/rest/v2/tasks/mongodb_kubernetes_operator_e2e_tests_e2e_test_feature_compatibility_version_13eb844c55774ce8a6de51edde1a66b4371f3ef6_21_03_25_15_57_45"
- :headers '(("Api-User" . "nikolas.de-giorgis") (acons "Api-Key" api_key ()))'
- :parser 'json-read
- :success (cl-function
- (lambda (&key data &allow-other-keys)
-   (message "I sent: %S" data ))))
-)
-
-
-(message "%s" (acons "Api-Key" api_key ()))
+(setq api_user (getenv "EVG_API_USER"))
+(if (or
+     (= (length api_key) 0)
+     (= (length api_user) 0)
+     )
+    (message "Please make sure to set the env variables $EVG_API_KEY and $EVG_API_USER")
+  (progn
+    (setq header (list
+                  (cons "Api-User" api_user)
+                  (cons "Api-Key" api_key)
+                  )
+          )
+    (request
+      (concatenate 'string "https://evergreen.mongodb.com/rest/v2/users/" api_user "/patches?limit=1")
+      :headers header
+      :parser 'json-read
+      :success (cl-function
+                (lambda (&key data &allow-other-keys)
+                  (progn
+                    (setq patch_id (assoc-default 'patch_id (svref data 0)) ))))
+      (request
+        (concatenate 'string "https://evergreen.mongodb.com/rest/v2/patches/" patch_id)
+        :type "PATCH"
+        :data (json-encode '(("priority" . 30) ))
+        :headers header
+        :parser 'json-read
+        )
+      )
+    )
+  )
