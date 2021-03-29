6b9efc093a91ec249705123e2fa88357fa3efc20
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2021-03-29 13:00:53 +0100

    Simple req, not actually working (need to figure out acons)

--- scratch/simple_req.el
+++ scratch/simple_req.el
@@ -1,9 +1,16 @@
 (require 'request)
 
+(setq api_key (getenv "EVG_API_KEY"))
+(if (= (length api_key) 0)
+      (message "Please make sure to set the env variable $EVG_API_KEY")
 (request
  "https://evergreen.mongodb.com/rest/v2/tasks/mongodb_kubernetes_operator_e2e_tests_e2e_test_feature_compatibility_version_13eb844c55774ce8a6de51edde1a66b4371f3ef6_21_03_25_15_57_45"
- :headers '(("Api-User" . "nikolas.de-giorgis") ("Api-Key" . "xxxxx"))'
+ :headers '(("Api-User" . "nikolas.de-giorgis") (acons "Api-Key" api_key ()))'
  :parser 'json-read
  :success (cl-function
  (lambda (&key data &allow-other-keys)
-             (message "I sent: %S" data ))))
+   (message "I sent: %S" data ))))
+)
+
+
+(message "%s" (acons "Api-Key" api_key ()))
