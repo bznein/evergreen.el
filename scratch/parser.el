06c70a3014a9d0467f4ef5f50e6eff5f6ea3b2e2
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2021-04-08 17:51:41 +0100

    Added task selector with bvs

--- scratch/parser.el
+++ scratch/parser.el
@@ -10,4 +10,53 @@
         )
         )
       )
-(message "%s" variants)
+;;(message "%s" variants)
+
+
+;; build task name lists
+(setq tasks())
+(loop for bv across (assoc-default 'tasks dict) do
+      (progn
+        (push (assoc-default 'name bv) tasks
+              )
+        )
+      )
+;;(message "%s" tasks)
+
+;; build task_groups name lists
+(setq task_groups())
+(loop for bv across (assoc-default 'task_groups dict) do
+      (progn
+        (push (assoc-default 'name bv) task_groups
+              )
+        )
+      )
+
+
+(setq task (completing-read "Task " tasks  nil t ""))
+
+;; get all bvs that contain this. Need to consider either direct task or groups!
+;;(message "%s" task)
+(setq matching_bvs ())
+(loop for bv across (assoc-default 'buildvariants dict) do
+      (progn
+        (loop for task_name across (assoc-default 'tasks bv) do
+              (progn
+                (setq task_real_name  (assoc-default 'name task_name))
+                (if (string= task_real_name task)
+                    (push (assoc-default 'name bv) matching_bvs)
+                  ;; Here I need to check if this name is a task group
+                  (progn
+                    (if (member task_real_name task_groups)
+                        ;; TODO here loop through the task and see it `task' is here!
+                        (message "%s is a task group" task_name );; then
+                      (message "%s is not a task group" task_name );; else
+                      )
+                    )
+                  )
+                )
+              )
+        )
+      )
+
+(message "%s" matching_bvs)
