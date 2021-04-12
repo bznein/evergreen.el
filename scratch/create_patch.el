9bc6329c4512f48d00c254cb26909db679424889
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2021-04-12 12:50:00 +0100

    Adds support for description when creating patches

---
+++ scratch/create_patch.el
@@ -0,0 +1,18 @@
+(defun mdb/create-patch ()
+  (interactive)
+  (setq api_key (getenv "EVG_API_KEY"))
+  (setq api_user (getenv "EVG_API_USER"))
+  (setq task_to_run (mdb/evg-get-task))
+  (setq build_variants (mdb/evg-get-matching-build-variants-for-task task_to_run))
+  (setq description (read-string "Description: "))
+  (shell-command
+   (concatenate 'string
+                "evergreen patch"
+                " -p " (replace-regexp-in-string "\n\\'" "" (mdb/get-current-repo))
+                " -t " task_to_run
+                " -v " (s-join " -v " build_variants)
+                " -d " description
+                " -f -y -u -browse"
+                )
+   )
+  )
